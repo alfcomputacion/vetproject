e9fd4aef02b67eb5463c3098f13cd4f7ba684a15
Jose Rosas <alfcomputacion@gmail.com>
2022-12-20 17:49:23 -0800

    registered job and aplicant

--- vetproj/jobs/admin.py
+++ vetproj/jobs/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from .models import Job, Aplicant
 # Register your models here.
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    model = Job
+    list_display = ['title', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj: None):
+        if obj:
+            return ('title', 'created', 'updated')
+        return ()
+
+
+@admin.register(Aplicant)
+class AplicantAdmin(admin.ModelAdmin):
+    model = Aplicant
+    list_display = ['first_name', 'last_name',
+                    'email', 'website',
+                    'employment_type', 'start_date',
+                    'available_days', 'desired_hourly_wage',
+                    'cover_letter', 'job', 'created', 'updated']
+
+    def get_readonly_fields(self, request, obj: None):
+        if obj:
+            return ('first_name', 'last_name',
+                    'email', 'website',
+                    'employment_type', 'start_date',
+                    'available_days', 'desired_hourly_wage',
+                    'cover_letter', 'job', 'created', 'updated')
+        return ()
